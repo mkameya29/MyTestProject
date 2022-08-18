cda221c322af22c8ae53a989c7157a4d6488ca23
Masaki Kameya <kameya@digitalfashion.jp>
2022-08-19 08:42:43 +0900

    add project files.

---
+++ MyTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestProject
+//
+//  Created by Masaki Kameya on 2022/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
