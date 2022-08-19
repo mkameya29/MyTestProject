970c9b71be71b581152295e3a3b1faffc597cb23
Masaki Kameya <mkameya29@gmail.com>
2022-08-19 09:56:31 +0900

    added push button

--- MyTestProject/ViewController.swift
+++ MyTestProject/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var pushButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.mainLabel.text = "Hello World"
     }
 
 
+    @IBAction func onPushButton(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "Hey", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "got it", style: .default, handler: { _ in
+            print("dismissed.")
+        }))
+        
+        present(alert, animated: true)
+    }
 }
 
