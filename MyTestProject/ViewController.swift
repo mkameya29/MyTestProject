9a2646d4f3e2c2366d9292ca1fded00f79f23a07
Masaki Kameya <mkameya29@gmail.com>
2022-08-25 16:48:16 +0900

    ARViewControllerと開始ボタン追加

--- MyTestProject/ViewController.swift
+++ MyTestProject/ViewController.swift
@@ -5,19 +5,24 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var pushButton: UIButton!
     @IBOutlet weak var testButton: UIButton!
-
+    @IBOutlet weak var arButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.mainLabel.text = "Hello World"
-    }
+        
+        //check AR capability
+        self.arButton.isHidden = !ARWorldTrackingConfiguration.isSupported
 
+    }
 
     @IBAction func onPushButton(_ sender: Any) {
         let alert = UIAlertController(title: nil, message: "Hey", preferredStyle: .alert)
@@ -28,14 +33,11 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
 
-    @IBAction func onTestButton(_ sender: Any) {
-        let alert = UIAlertController(title: NSLocalizedString("TEST_TITLE", comment: "test"), message: "test", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "got it", style: .default, handler: { _ in
-            print("dismissed.")
-        }))
+    
+    /// AR表示画面を開く
+    /// - Parameter sender: ARスタートボタン
+    @IBAction func onARButton(_ sender: Any) {
         
-        present(alert, animated: true)
-
     }
 }
 
